e6635fce7b88f14074b46ca5887146a2dea7f282
MChan127 <matthew12792@gmail.com>
2018-02-11 15:25:00 -0800

    fixed key

--- src/app.ts
+++ src/app.ts
@@ -69,7 +69,7 @@ class Gamogori {
         this.game.state.add("lvl2-4", DreamTwoToFour);
         this.game.state.add("lvl2-4t", DreamTwoFourText);
         this.game.state.add("ending", DreamCliffHangerText);
-        this.game.state.start("lvl1-1t");
+        this.game.state.start("lvl2-1t");
     }
 
     private update(): void {
